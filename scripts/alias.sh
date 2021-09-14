e9b2fd88bc48af3a63d964a8d10f62c9a8ef0bbd
Crow314 <master@crow31415.net>
2021-09-14 20:52:02 +0900

    Setup migrate

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -3,9 +3,11 @@ project_name='entry-notificator-development'
 alias docker-compose="docker-compose -f deployments/development/docker-compose.yml -p $project_name"
 
 alias build="docker-compose build"
-alias up="docker-compose up"
+alias up="docker-compose up app"
 alias stop="docker-compose stop"
 alias down="docker-compose down"
+
+alias migrate="docker-compose run --rm migrate"
 
 alias run="docker-compose run --rm"
 alias exec="docker-compose exec"
