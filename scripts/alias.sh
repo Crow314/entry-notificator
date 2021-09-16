7fd8b417f1e00ae0276a40af5e04c84812356838
Crow314 <master@crow31415.net>
2021-09-17 00:35:21 +0900

    Set alias "migrate-create"

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -9,6 +9,7 @@ alias down="docker-compose down"
 
 alias migrate="docker-compose run --rm migrate"
 alias migrate-run="migrate -database postgres://postgres:postgres@postgres:5432/development?sslmode=disable -path /migrations"
+alias migrate-create="migrate create -ext sql -dir /migrations -seq"
 
 alias run="docker-compose run --rm"
 alias exec="docker-compose exec"
